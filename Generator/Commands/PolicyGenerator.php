87aae76f205afd75d05a15ab474ae15fa792c111
Dmitry Vovk <dmitry.vovk@uinno.io>
2022-11-01 22:19:32 +0200

    fix(Component): update signature;

--- Generator/Commands/PolicyGenerator.php
+++ Generator/Commands/PolicyGenerator.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Apiato\Core\Generator\Commands;
 
 use Apiato\Core\Generator\GeneratorCommand;
@@ -45,19 +47,19 @@ class PolicyGenerator extends GeneratorCommand implements ComponentsGenerator
      */
     protected string $stubName = 'policy.stub';
 
-    public function getUserInputs(): ?array
+    public function getUserInputs(): array
     {
         return [
             'path-parameters' => [
-                'section-name' => $this->sectionName,
+                'section-name'   => $this->sectionName,
                 'container-name' => $this->containerName,
             ],
             'stub-parameters' => [
-                '_section-name' => Str::lower($this->sectionName),
-                'section-name' => $this->sectionName,
+                '_section-name'   => Str::lower($this->sectionName),
+                'section-name'    => $this->sectionName,
                 '_container-name' => Str::lower($this->containerName),
-                'container-name' => $this->containerName,
-                'class-name' => $this->fileName,
+                'container-name'  => $this->containerName,
+                'class-name'      => $this->fileName,
             ],
             'file-parameters' => [
                 'file-name' => $this->fileName,
